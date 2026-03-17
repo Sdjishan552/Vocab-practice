997c7007ef4c96202108aa97c89d5701e91b657c
Syed Jishan Nawaz <sdjishan52@gmail.com>
2026-03-17 14:03:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("[DEBUG]:", message);
   }
  
-
+ 
   /***********************
     SECTION SWITCHING
   ************************/
